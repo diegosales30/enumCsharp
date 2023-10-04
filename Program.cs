fa587391df12e4308e2dc6827890e4a2b6eecc4e
diegosales30 <diegofelipesales23@gmail.com>
2023-10-04 17:50:52 -0300

    enum c#

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using EnumCsharp.Entities;
+using EnumCsharp.Entities.Enums;
+
+namespace EnumCsharp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Order order = new Order {
+        Id = 1080,
+        Moment = DateTime.Now,
+        Status = OrderStatus.PendingPayment
+      };
+
+      Console.WriteLine(order);
+
+      //coverter enum para string
+      string txt = OrderStatus.PendingPayment.ToString();
+      Console.WriteLine("string: " +txt);
+
+      //converter string para enum
+      OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+      Console.WriteLine("enum: " +os);
+    }
+  }
+}
